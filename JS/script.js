cff4a81c625c91337c13f3783c4f71261e2db615
DevMERN <abiodunDev@gmail.com>
2019-09-19 06:20:18 +0100

    Adding a validation for the sign-up form and adding to the styling of the form

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
+// Accessing the DOM Element
+const loginButton = document.getElementById('login-button');
+const errorMsg = document.getElementById('error-message');
+const passwordInput = document.getElementById('password-input');
+const confirmPassword = document.getElementById('confirm-password');
 
+
+// Listener to blue event when tabbed away from the confirm password
+confirmPassword.addEventListener('blur', (event) => {
+	event.preventDefault();
+	if (passwordInput.value === confirmPassword.value) {
+		passwordInput.style.border = 'thin solid green';
+		confirmPassword.style.border = 'thin solid green';
+		errorMsg.style.display = 'none';
+	} else {
+		passwordInput.style.border = 'thin solid red';
+		confirmPassword.style.border = 'thin solid red';
+		errorMsg.style.display = 'inline';
+	}
+});
